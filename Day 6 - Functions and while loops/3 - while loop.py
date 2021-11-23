945c99fea4a1bf661605d4df870e46114fa221df
Yuri Valenca <yvc@cin.ufpe.br>
2021-11-23 13:37:48 -0300

    Day 6

--- Day 6 - Functions and while loops/3 - while loop.py
+++ Day 6 - Functions and while loops/3 - while loop.py
@@ -11,4 +11,4 @@ while algo == True:
     print("oi")
     algo == False  # No for, a condição de parada é dada no começo, com range() ou limitando ao número de itens numa lista, no while, a condição de parada fica dentro do bloco a ser repetido, caso não haja uma condição de parada, o código rodará pra sempre. Nesse caso, o bloco seria repetido apenas uma vez, pois assim que ele é executado, a variável algo vira False, e na comparação do while, a condição de repetição algo == True não é mais verdadeira, com isso, o bloco não é repetido de novo
 
-# While loops são bastante usados quando não é sabido a quantidade de vezes que o bloco irá ser repetido, ou se a condição de parada se altera!
+# While loops são bastante usados quando não é sabido a quantidade de vezes que o bloco irá ser repetido, ou se a condição de parada se altera
