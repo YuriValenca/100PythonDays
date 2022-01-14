9ed46d6396ce7d61e39bd17590910a0ae62196a7
Yuri Valença <yvc@cin.ufpe.br>
2022-01-14 16:45:29 -0300

    Day 02

--- Day 02 - String manip and data types/1 - data types.py
+++ Day 02 - String manip and data types/1 - data types.py
@@ -5,7 +5,7 @@ print("Oi!"[2]) #O índice presente dentro dos colchetes indica para o programa 
 
 # Int (números sem parte decimal)
 print(1 + 2) #Os int diferenciam-se de uma string por não terem aspas duplas os envolvendo. É assim que o programa saberá se deve concatenar 2 textos ou somar 2 números
-print(123_456) #Para expressar números grandes, ao invés de usar vírgula - padrão americano - ou ponto - padrão brasileiro - é possível usar underline, o programa irá ignorar esse caracter, porém fica mais fácil de identificar qual o número
+print(123_456) #Para expressar números grandes, ao invés de usar vírgula - padrão americano - ou ponto - padrão brasileiro - é possível usar underline, o programa irá ignorar esse caracter, porém fica mais fácil de identificar qual o número.
 
 # Float (números com parte decimal)
 print(3.14159) #Lembrar que, devido ao padrão americano, deve-se usar . ao invés de , para dividir o valor decimal de um número
