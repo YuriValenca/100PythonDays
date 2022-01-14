31edcd9c27224c6fb01ff2e78546d0cba522ae80
Yuri Valença <yvc@cin.ufpe.br>
2022-01-14 16:46:46 -0300

    Day 03

--- Day 03 - Control flow and logical ops/3 - nested if else.py
+++ Day 03 - Control flow and logical ops/3 - nested if else.py
@@ -5,7 +5,7 @@ if altura > 1.2:
     print("Você pode andar na montanha-russa!")
     idade = int(input("Para continuar a compra do seu ingresso, nos diga qual sua idade: "))
     if idade > 18:      
-        #Essa é uma condição aninhada, ela só será executada se o bloco dentro do if exterior for verdadeiro, se o condicional externo for para o bloco de códigos do "else", nada do que está dentro do if externo seria executado, INCLUINDO o if/else aninhado
+        #Essa é uma condição aninhada, ela só será executada se o bloco dentro do if exterior for verdadeiro, se o condicional externo for para o bloco de códigos do "else", nada do que está dentro do if externo seria executado, INCLUINDO o if/else aninhado.
         print("O valor do ingresso é 12 reais")
     elif idade > 12 and idade <= 18:     
         #O condicional elif serve para que o else/if não seja apenas binário, múltiplos elifs podem ser invocados à medida que o código necessite de mútiplas condições diferentes
