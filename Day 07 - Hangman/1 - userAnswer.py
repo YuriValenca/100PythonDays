b1988136ee29ef3d6bcdc2d6b9435550edf15000
Yuri Valença <yvc@cin.ufpe.br>
2022-01-15 17:23:05 -0300

    Day 07

--- Day 07 - Hangman/1 - userAnswer.py
+++ Day 07 - Hangman/1 - userAnswer.py
@@ -2,7 +2,7 @@ import random
 
 palavrasForca = []
 
-# Em prol de melhorar a experiência do jogo, baixei um arquivo com uma lista de palavras em português. No entando, isso requereu que fosse utilizado uma propriedade ainda não vista e que não será atacada nessa lição da forca: o open file. Assim que esse conteúdo for mencionado no curso, eu modificarei esse código para referir onde está a teoria por trás do bloco de código abaixo. Mas é bom saber que ele serve para identificar cada palavra do arquivo e colocá-las numa lista, separando cada uma pelo "\n", operação que quem faz é o line.strip(). O for irá percorrer o arquivo e sair armazenando cada palavra numa lista vazia, que será a utilizada no jogo da forca final
+# Em prol de melhorar a experiência do jogo, baixei um arquivo com uma lista de palavras em português. No entando, isso requereu que fosse utilizado uma propriedade ainda não vista e que não será atacada nessa lição da forca: o open file. Assim que esse conteúdo for mencionado no curso, eu modificarei esse código para referir onde está a teoria por trás do bloco de código abaixo. Mas é bom saber que ele serve para identificar cada palavra do arquivo e colocá-las numa lista, separando cada uma pelo "\n", operação que quem faz é o line.strip(). O for irá percorrer o arquivo e sair armazenando cada palavra numa lista vazia, que será a utilizada no jogo da forca final.
 
 f = open('C:\Portfólio\Python course\Day 7 - Hangman\ListaDePalavras.txt', 'r')
 
