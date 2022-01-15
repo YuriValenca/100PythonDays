c05bb26c1b00e237f71685b53ffde38afb6d4f10
Yuri Valença <yvc@cin.ufpe.br>
2022-01-15 17:22:08 -0300

    Day 06

--- Day 06 - Functions and while loops/1 - functions.py
+++ Day 06 - Functions and while loops/1 - functions.py
@@ -1,6 +1,6 @@
 # Ao longo dos códigos anteriores, diversas funções foram utilizadas múltiplas vezes. Algumas executadas devido à importação de um módulo, outras que são do próprio Python. As funções são identificadas pelos parênteses, como print(), len(), int(), etc.
 
-# É possível ainda criar próprias funções, seguindo o padrão def minhaFunção(argumentos) e dentro dela, de maneira IDENTADA, tem o bloco de código que ela realiza. Para invocar a função, basta chamá-la no código: minhaFunção(argumentos). Lembrando que uma função pode ter zero ou mais argumentos
+# É possível ainda criar próprias funções, seguindo o padrão def minhaFunção(argumentos) e dentro dela, de maneira IDENTADA, tem o bloco de código que ela realiza. Para invocar a função, basta chamá-la no código: minhaFunção(argumentos). Lembrando que uma função pode ter zero ou mais argumentos.
 
 def myFunction():  # Definição da função
     print("Oi!")
