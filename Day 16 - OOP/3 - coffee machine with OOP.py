a46fa2d04ecf700f712eaa478c8e2730de3fffff
Yuri Valença <yvc@cin.ufpe.br>
2022-01-24 14:52:29 -0300

    Day 16 - WIP

---
+++ Day 16 - OOP/3 - coffee machine with OOP.py
@@ -0,0 +1,2 @@
+# Com o conhecimento visto de OOP (Object oriented programming), agora é possível reescrever o código da máquina de café utilizando os conceitos de objetos, classes, funções e métodos
+
