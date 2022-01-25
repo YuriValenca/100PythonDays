294b4f993acdeb11b5ab0db748bccfac3aeeef75
YuriValenca <yvc@cin.ufpe.br>
2022-01-24 22:10:39 -0300

    Day 12

--- Day 12 - Scope/3 - global constants.py
+++ Day 12 - Scope/3 - global constants.py
@@ -1,7 +1,7 @@
 # O caso das constantes globais é um pouco diferente, pois, como é uma constante, não temos intenção de modificá-la durante o código
 # Por exemplo:
 piValue = 3.14159
-# O valor de pi será sempre o mesmo, só iremos acessá-lo, e não modificá-lo
+# O valor de pi será sempre o mesmo, só iremos acessá-lo, e não modificá-lo.
 # O ideal então é colocar o nome da constante todo em maiúsculo, não por alguma mudança interna no funcionamento do código, mas sim para que você se lembre, ao acessar aquela constante, que ela deve ser apenas lida, e não modificada!
 
 PI = 3.14159
